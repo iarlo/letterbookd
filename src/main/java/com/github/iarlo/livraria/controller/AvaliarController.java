06e7448593353786bc21ed9a84ecbaef28c1ac72
masmsantana <marco.desantana@dcomp.ufs.br>
2025-08-23 20:38:33 -0300

    Criando AvaliarController.java
    Implementação do Controller "AvaliarController"  responsável por gerenciar avaliações de livros.

---
+++ src/main/java/com/github/iarlo/livraria/controller/AvaliarController.java
@@ -0,0 +1,33 @@
+package com.github.iarlo.livraria.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.github.iarlo.livraria.model.Livro;
+import com.github.iarlo.livraria.model.Review;
+import com.github.iarlo.livraria.service.LivrariaService;
+
+@Controller
+@RequestMapping("/avaliar")
+public class AvaliarController {
+    private final LivrariaService livrariaService = new LivrariaService();
+
+    @GetMapping("/{id}")
+    public String exibirFormularioAvaliar(@PathVariable int id, Model model) {
+        Livro livro = livrariaService.buscarPorId(id);
+        model.addAttribute("livro", livro);
+        return "avaliar";
+    }
+
+    @PostMapping("/{id}")
+    public String avaliar(@ModelAttribute Review review, @PathVariable int id) {
+        livrariaService.adicionarReview(id, review);
+        return "redirect:/livros/{id}";
+    }
+
+}
