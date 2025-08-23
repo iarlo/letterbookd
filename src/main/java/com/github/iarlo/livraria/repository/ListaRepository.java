e581d8c6ea642c4cb2d566bba16a522a15816db5
Bruno Sá <53959303+iarlo@users.noreply.github.com>
2025-08-23 18:53:20 -0300

    refactor(Lista): Remove a necessidade do modelo, passando a responsabilidade pro repository

--- src/main/java/com/github/iarlo/livraria/repository/ListaRepository.java
+++ src/main/java/com/github/iarlo/livraria/repository/ListaRepository.java
@@ -1,7 +1,10 @@
 package com.github.iarlo.livraria.repository;
 
-import com.github.iarlo.livraria.model.Lista;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.github.iarlo.livraria.model.Livro;
+
 /**
  * Implementa o padrão de projeto singleton para garantir que a classe seja
  * criada apenas uma vez. Isso é bom pra gente que vai armazenar dados na
@@ -11,41 +14,50 @@ import com.github.iarlo.livraria.model.Livro;
  * https://refactoring.guru/pt-br/design-patterns/singleton/java/example#example-1
  */
 public class ListaRepository {
-    
+
     private static ListaRepository instancia;
-    private final Lista lista = new Lista();
-  /**
+
+    private List<Livro> livrosLidos = new ArrayList<>();
+    private List<Livro> livrosLista = new ArrayList<>();
+
+    /**
      * O constructor privado serve pra impedir que a classe seja acessada
      * diretamente
      */
-    private ListaRepository(){}
+    private ListaRepository() {
+    }
+
     /**
      * Se a instancia já tiver sido definida, retorna ela, se não, cria uma nova
      */
-    public static ListaRepository getInstance(){
-        if(instancia == null){
+    public static ListaRepository getInstance() {
+        if (instancia == null) {
             instancia = new ListaRepository();
         }
         return instancia;
     }
 
-    public void adicionarLivroLido(Livro livro) {
-        lista.addLivroLido(livro);
+    public List<Livro> getLivrosLidos() {
+        return livrosLidos;
     }
 
-    public void adicionarLivroNaLista(Livro livro) {
-        lista.addLivroLista(livro);
+    public List<Livro> getLivrosLista() {
+        return livrosLista;
+    }
+
+    public void addLivroLido(Livro livro) {
+        livrosLidos.add(livro);
+    }
+
+    public void addLivroLista(Livro livro) {
+        livrosLista.add(livro);
     }
 
     public void removeLivroLido(Livro livro) {
-        lista.removeLivroLido(livro);
+        livrosLidos.remove(livro);
     }
 
     public void removeLivroLista(Livro livro) {
-        lista.removeLivroLista(livro);
-    }
-
-    public Lista getLista() {
-        return lista;
+        livrosLista.remove(livro);
     }
 }
